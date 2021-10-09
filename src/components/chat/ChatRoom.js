c7b44226436f6f58e510ef179383ffe1ddf6f737
ar[arnau321 <arnau123@att.net>
2021-10-09 19:51:30 -0400

    Change socket to use state

--- src/components/chat/ChatRoom.js
+++ src/components/chat/ChatRoom.js
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useParams } from 'react-router-dom'
+import { io } from 'socket.io-client'
 
-export default function ChatRoom ({ socket, msgAlert, user }) {
+export default function ChatRoom ({ msgAlert, user }) {
   // set state for message
   const [currentMessage, setCurrentMessage] = useState('')
 
+  // sets socket var
+  const socket = useRef(io('ws://localhost:4741'))
   // allows to use room and name from url
   const { room, name } = useParams()
 
-  // on page load gets message from server
   useEffect(() => {
-    console.log('in useEffect')
-    socket.on('message', (data) => {
-      console.log('data from server', data)
+    socket.current.emit('addUser', user._id)
+    socket.current.on('getUsers', users => {
+      console.log('users', users)
     })
-  }, [socket])
+  }, [user])
 
   // Sends message with socket
   const sendMessage = async () => {
